176ae5c366af82eba98524e5ff0c57b83f568eb1
Aviral Saxena <avisaxena33@gmail.com>
2020-10-22 00:45:18 -0500

    cleaned up initial postgres code to a method

--- server.py
+++ server.py
@@ -2,34 +2,21 @@ import sys
 import boto3
 import psycopg2
 
-# ACCESS_KEY = 'AKIA6FN67KREATPJLKK4'
-# SECRET_KEY = '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'
-
+# Environment variables
 ENDPOINT="stock-screener-2.c1h93r1ybkd8.us-west-2.rds.amazonaws.com"
 PORT="5432"
 USR="Administrator"
 REGION="us-west-2"
 DBNAME="postgres"
 
+# This looks bad I know C:
 MASTER_USERNAME = 'buffoon_squad'
 MASTER_PASSWORD = 'Sharingan_Amaterasu33'
 
-
-#gets the credentials from .aws/credentials
-# session = boto3.Session(profile_name='dev_config')
-# client = boto3.client('rds')
-
-# token gen not working for IAM admin user -- will be using DB master user and pass for now
-# token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USR, Region=REGION)   
-
-try:
-    conn = psycopg2.connect(host=ENDPOINT, port=PORT, dbname=DBNAME, user=MASTER_USERNAME, password=MASTER_PASSWORD)
-    cur = conn.cursor()
-    cur.execute("""SELECT now()""")
-    query_results = cur.fetchall()
-    # cur.execute("CREATE TABLE swag (name VARCHAR PRIMARY KEY);")
-    cur.execute("SELECT * FROM swag;")
-    cur.fetchone()
-    print(query_results)
-except Exception as e:
-    print("Database connection failed due to {}".format(e))   
+# Attempts to connect to database and returns connection object if successsful
+# Else will throw an exception
+def connect_to_postgres(): 
+    try:
+        return psycopg2.connect(host=ENDPOINT, port=PORT, dbname=DBNAME, user=MASTER_USERNAME, password=MASTER_PASSWORD)
+    except Exception as e:
+        print("Database connection failed due to {}".format(e))   

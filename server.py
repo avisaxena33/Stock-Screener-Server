dc1bed028f00925633492939e14a2a6ab16a9990
Aviral Saxena <avisaxena33@gmail.com>
2020-10-23 00:56:42 -0500

    added 2 stored procedures and 2 rest api endpoints to add and remove trackers

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import sys
 import boto3
 import psycopg2
+from flask import Flask
+
+# Flask class reference
+app = Flask(__name__)
 
 # Environment variables
 ENDPOINT="stock-screener-2.c1h93r1ybkd8.us-west-2.rds.amazonaws.com"
@@ -14,9 +18,41 @@ MASTER_USERNAME = 'buffoon_squad'
 MASTER_PASSWORD = 'Sharingan_Amaterasu33'
 
 # Attempts to connect to database and returns connection object if successsful
-# Else will throw an exception
 def connect_to_postgres(): 
     try:
         return psycopg2.connect(host=ENDPOINT, port=PORT, dbname=DBNAME, user=MASTER_USERNAME, password=MASTER_PASSWORD)
     except Exception as e:
         print("Database connection failed due to {}".format(e))   
+
+# testing flask rest calls
+@app.route("/")
+def hello():
+    return 'hello'
+
+# Tracks a new ticker if not already tracked
+@app.route('/add_tracker/<string:ticker>')
+def add_tracker(ticker):
+    connection = connect_to_postgres()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("CALL add_tracker(%s);", (ticker,))
+    except Exception as e:
+        return str(e)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return 'Successfully added the selected ticker!'
+
+# Removes a ticker that is being tracked
+@app.route('/remove_tracker/<string:ticker>')
+def remove_tracker(ticker):
+    connection = connect_to_postgres()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("CALL remove_tracker(%s);", (ticker,))
+    except Exception as e:
+        return str(e)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return 'Successfully removed the selected ticker!'
